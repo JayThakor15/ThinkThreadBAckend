9ec1a3319e9565f6117ad517cd2581623b21b056
Jay Thakor <jaysinhdarbar730@gmail.com>
2025-08-03 08:58:23 +0530

    fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: "https://talentthread.netlify.app/",
+    origin: ["http://localhost:5173", "https://talentthread.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
